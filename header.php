01bdd58eece56b6382e47420ec8ffc4590234726
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-09-16 16:12:42 -0400

    #36 customizing main menu

--- header.php
+++ header.php
@@ -34,30 +34,34 @@
             <section class="top-bar">
                 <div class="container">
                     <div class="row">
-                        <div class="brand col-3">Logo</div>
-                        <div class="second-column col-9 ">
-                            <div class="account">Account</div>
-                            <nav class="navbar navbar-expand-md navbar-light" role="navigation">
+                        <div class="brand col-md-3 col-12 col-lg-2 text-center text-md-left">Logo</div>
+                        <div class="second-column col-md-9 col-12 col-lg-10">
+                            <div class="row">
 
-                                <!-- Brand and toggle get grouped for better mobile display -->
-                                <button class=" main-menu navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
+                                <div class="account col-12">Account</div>
+                                <div class="col-12">
+                                    <nav class="navbar navbar-expand-md navbar-light" role="navigation">
+                                        <!-- Brand and toggle get grouped for better mobile display -->
+                                        <button class=" main-menu navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+                                            <span class="navbar-toggler-icon"></span>
+                                        </button>
 
-                                <?php
-                                wp_nav_menu(array(
-                                    'theme_location'    =>  'style_maven_main_menu',
-                                    'depth'             => 3,
-                                    'container'         => 'div',
-                                    'container_class'   => 'collapse navbar-collapse',
-                                    'container_id'      => 'bs-example-navbar-collapse-1',
-                                    'menu_class'        => 'nav navbar-nav',
-                                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                                    'walker'            => new WP_Bootstrap_Navwalker(),
-                                ));
-                                ?>
+                                        <?php
+                                        wp_nav_menu(array(
+                                            'theme_location'    =>  'style_maven_main_menu',
+                                            'depth'             => 3,
+                                            'container'         => 'div',
+                                            'container_class'   => 'collapse navbar-collapse',
+                                            'container_id'      => 'bs-example-navbar-collapse-1',
+                                            'menu_class'        => 'nav navbar-nav',
+                                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                                            'walker'            => new WP_Bootstrap_Navwalker(),
+                                        ));
+                                        ?>
+                                    </nav>
+                                </div>
 
-                            </nav>
+                            </div>
                         </div>
                     </div>
                 </div>
