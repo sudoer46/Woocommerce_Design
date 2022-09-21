6e7a100d9dce3ca35dc9013be7924093d6a70073
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-09-21 12:27:53 -0400

    #41 Link to User Account

--- header.php
+++ header.php
@@ -40,13 +40,42 @@
                         <div class="brand col-md-3 col-12 col-lg-2 text-center text-md-left">Logo</div>
                         <div class="second-column col-md-9 col-12 col-lg-10">
                             <div class="row">
-                                <div class="account col-12">
-                                    <div class="cart text-right">
-                                        <a href="<?php echo wc_get_cart_url() ?>"><span class="cart-icon"></span></a>
-                                        <span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+                                <!-- check if woocommerce exists -->
+                                <?php if (class_exists("WooCommerce")) : ?>
+                                    <div class="account col-12">
+                                        <div class="navbar-expand">
+                                            <ul class="navbar-nav float-left">
 
+                                                <?php
+                                                if (is_user_logged_in()) {
+                                                ?>
+                                                    <li>
+                                                        <a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))) ?>" class="nav-link">My Account</a>
+                                                    </li>
+                                                    <li>
+                                                        <!-- Pass the url to the account page -->
+                                                        <a href="<?php echo esc_url(wp_logout_url(get_permalink(get_option('woocommerce_myaccount_page_id')))) ?>" class="nav-link">Log Out</a>
+                                                    </li>
+                                            </ul>
+
+                                        <?php } else {
+                                        ?>
+                                            <li>
+                                                <a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))) ?>" class="nav-link">Login/Register</a>
+                                            </li>
+
+                                        <?php } ?>
+
+
+                                        </div>
+                                        <div class="cart text-right">
+                                            <a href="<?php echo wc_get_cart_url() ?>"><span class="cart-icon"></span></a>
+                                            <span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+
+                                        </div>
                                     </div>
-                                </div>
+
+                                <?php endif; ?>
                                 <div class="col-12">
                                     <nav class="navbar navbar-expand-md navbar-light" role="navigation">
                                         <!-- Brand and toggle get grouped for better mobile display -->
