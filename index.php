c50224f90d3c1791d081282e749fc9b0e5518c17
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-10-20 12:20:33 -0400

    #59 Modify blog post index

--- index.php
+++ index.php
@@ -28,9 +28,39 @@ get_header(); ?>
 
                         // do stuff ...>
                 ?>
-                        <article>
-                            <h2><?php the_title(); ?></h2>
-                            <div><?php the_content(); ?></div>
+                        <article <?php post_class(); ?>>
+                            <h2>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+                            <div class="post-thumbnail">
+                                <?php
+                                //check for thumbnail and render it
+                                if (has_post_thumbnail()) :
+                                ?>
+                                    <a href="<?php the_permalink() ?>">
+                                        <?php
+                                        the_post_thumbnail('style-maven-blog', array('class' => 'img-fluid'));
+                                        ?>
+                                    </a>
+                                <?php
+                                endif;
+                                ?>
+                            </div>
+                            <div class="meta">
+                                <p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
+                                    <br>
+                                    Categories: <span><?php the_category(" "); ?></span>
+                                    <br>
+                                    <?php
+                                    if (has_tag()) :
+                                    ?>
+                                        Tags: <span><?php the_tags("", ", ", ""); ?></span>
+                                    <?php endif; ?>
+                                </p>
+                            </div>
+                            <div><?php the_excerpt(); ?></div>
                         </article>
 
                     <?php
