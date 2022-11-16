5c1a44fe435f8a1009969f3fff86d9ad7bcaa20c
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-11-16 12:22:36 -0500

    #61 comments

--- page.php
+++ page.php
@@ -32,8 +32,19 @@ get_header(); ?>
                     <article class="col">
                         <h1><?php the_title(); ?></h1>
                         <div><?php the_content(); ?></div>
-                    </article>
+                        <?php
+                        /**
+                         * enabling comments
+                         * if one of the conditions are true, comments get displayed
+                         * requires that comments.php is in the root of the foulder
+                         */
+                        if (comments_open() || get_comments_number()) :
+                            comments_template();
+                        endif;
 
+                        ?>
+
+                    </article>
                 <?php
                 endwhile;
 
--- single.php
+++ single.php
@@ -18,22 +18,47 @@ get_header();
                 while (have_posts()) : the_post();
                     // do stuff ...
                 ?>
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+
+                    <article id='post-<?php the_ID(); ?>' <?php post_class(); ?>>
                         <header>
                             <h1><?php the_title(); ?></h1>
-                            <div class="meta">
-
+                            <div class='meta'>
+                                <p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
+                                    <br />
+                                    <?php if (has_category()) : ?>
+                                        Categories: <span><?php the_category(' '); ?></span><br />
+                                    <?php endif; ?>
+                                    <?php if (has_tag()) : ?>
+                                        Tags: <span><?php the_tags('', ', '); ?></span><br />
+                                    <?php endif; ?>
+                                </p>
                             </div>
-
-
+                            <div class='post-thumbnail'>
+                                <?php
+                                if (has_post_thumbnail()) :
+                                    the_post_thumbnail('style-maven-blog', array('class' => 'img-fluid'));
+                                endif;
+                                ?>
+                            </div>
                         </header>
-                        <div class="content">
+                        <div class='content'>
                             <?php the_content(); ?>
                         </div>
-
-
                     </article>
+
+
                 <?php
+                    /**
+                     * enabling comments
+                     * if one of the conditions are true, comments get displayed
+                     * requires that comments.php is in the root of the foulder
+                     */
+                    if (comments_open() || get_comments_number()) :
+                        comments_template('');
+                    endif;
+
                 endwhile;
 
                 ?>
